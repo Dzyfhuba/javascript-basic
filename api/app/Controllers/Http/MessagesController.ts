3a627f900bd92cc372c7552ac79cf434a7c6576e
Dzyfhuba <hafidz21ub@gmail.com>
2022-10-07 10:43:58 +0700

    add index and show method

--- api/app/Controllers/Http/MessagesController.ts
+++ api/app/Controllers/Http/MessagesController.ts
@@ -1,29 +1,61 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Message from 'App/Models/Message'
+import Logger from '@ioc:Adonis/Core/Logger'
 
 export default class MessagesController {
-  public async index ({response}: HttpContextContract) {
-    const messages = await Message.query().orderBy('updated_at', 'desc')
+  public async index ({response, request}: HttpContextContract) {
+    const {name, message} = request.qs() as {name: string, message: string}
+    try {
+      let messages
+      Logger.info(`name or message: ${name || message}`)
+      if (name || message) {
+        messages = await Message
+          .query()
+          .whereILike('name', `%${name}%`)
+          .orWhereILike('message', `%${message}%`)
+      } else {
+        messages = await Message
+          .query()
+          .orderBy('updated_at', 'desc')
+      }
 
-    return response.status(200).json(messages)
+      return response.status(200).json(messages)
+    } catch (error) {
+      return response.internalServerError(error)
+    }
   }
 
-  public async create ({}: HttpContextContract) {}
+  public async create ({response}: HttpContextContract) {
+    return response.methodNotAllowed()
+  }
 
   public async store ({request, response}: HttpContextContract) {
-    const {name, message} = request.body()
+    try {
+      const {name, message} = request.body()
 
-    const data = await Message.create({
-      name,
-      message,
-    })
+      const data = await Message.create({
+        name,
+        message,
+      })
 
-    return response.status(201).json({
-      ...data.$attributes,
-    })
+      return response.status(201).json({
+        ...data.$attributes,
+      })
+    } catch (error) {
+
+    }
   }
 
-  public async show ({}: HttpContextContract) {}
+  public async show ({request, response}: HttpContextContract) {
+    const {id} = request.params()
+    try {
+      const message = await Message.findOrFail(id)
+
+      return response.status(200).json(message)
+    } catch (error) {
+      return response.internalServerError()
+    }
+  }
 
   public async edit ({}: HttpContextContract) {}
 
